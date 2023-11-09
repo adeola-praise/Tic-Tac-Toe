e5b597ea25604981db3dea7ffda07b1f60f28b9b
adeola-praise <68433446+adeola-praise@users.noreply.github.com>
2023-11-09 11:55:22 +0100

    Add Gameplay Logic

--- comment.js
+++ comment.js
@@ -1,3 +1,4 @@
+// First I get the users name and preferred mark. The first user is marked X and the second user is marked O
 let players = [];
 
 let playerX = prompt("Player X name:  ");
@@ -5,6 +6,8 @@ createNewPlayer(playerX);
 
 let playerO = prompt("PlayerO name: ");
 createNewPlayer(playerO);
+
+// Now that we have the names of the players, we create players from this input
 
 function createPlayer(name) {
   let _mark = "X";
@@ -19,8 +22,12 @@ function createPlayer(name) {
   return { name, getMarker, setMarker };
 }
 
+// Gets the players input name, create a new player and set their markers
 function createNewPlayer(name) {
   let newPlayer = createPlayer(name);
+
+  //assign marker to player
+  // Check the array of players, if the length of player is 1, that means there is already a player "X", now assign this new player mark "0", else if there is no player, just push this new player into theplayers array.
 
   if (players.length >= 1) {
     newPlayer.setMarker("O");
@@ -29,6 +36,8 @@ function createNewPlayer(name) {
   players.push(newPlayer);
   alert(`${name}, your marker is ${newPlayer.getMarker()}`);
 }
+
+// Now we need a board on which the players can play on. Th board consists of three rows and three columns. The board would be a 2D array
 
 let gameBoard = (function () {
   const _gameBoard = [];
@@ -39,10 +48,6 @@ let gameBoard = (function () {
       _gameBoard[i][j] = " ";
     }
   }
-
-  let displayGameBoard = () => {
-    console.table(_gameBoard);
-  };
 
   let getGameBoard = () => {
     return _gameBoard;
@@ -56,8 +61,12 @@ let gameBoard = (function () {
     _gameBoard[row][column] = marker;
   };
 
-  return { getGameBoard, getBoardCell, markBoardCell, displayGameBoard };
+  return { getGameBoard, getBoardCell, markBoardCell };
 })();
+
+// console.log(gameBoard.getGameBoard());
+
+// Now players need to add markers to the gameBoard. Players need to access each cell in the gameboard, there are nine in total and players can add their markers to these cells. The only rule is that you can't add a mark to a cell that already has a mark
 
 function placeMark(player, row, column) {
   console.log(player);
@@ -71,9 +80,11 @@ function placeMark(player, row, column) {
   }
 }
 
+// check for empty cells
 function checkForEmptyCell() {
   let board = gameBoard.getGameBoard();
 
+  // Use the array.some method to check through the rows and columns for empt cells
   let isAnyCellEmpty = board.some((row) =>
     row.some((column) => column === " ")
   );
@@ -83,20 +94,28 @@ function checkForEmptyCell() {
 
 console.log(checkForEmptyCell());
 
+// Now how do the players play the game. I want to keep prompting for input from the players until all cells are filled up
+
 let currentPlayer = players[0];
 
 function checkPlayerTurn(player) {
+  // I need to check if it is the player's turn.
+  // Player X is always the first player to play. I need to check the last input value pushed into the gameboard array. If the input value is an X, then it is player O's turn, if it is an O, then it is player X's turn
+  // I can do this by storing the last input pushed into the array
   let previous_player = player;
+  console.log(`Previous player is ${player.name}`);
 
-  if (previous_player.getMarker() == "X") {
-    console.log(currentPlayer);
-    currentPlayer = players[1];
-  } else {
-    currentPlayer = players[0];
-  }
+  // if (previous_player.getMarker == "X") {
+  //   console.log(currentPlayer);
+  //   currentPlayer = players[1];
+  // } else {
+  //   currentPlayer = players[0];
+  // }
 }
 
 function receivePlayerInput() {
+  console.log("Input Received");
+
   let player_input = prompt(
     "Seperate selected row and column with a comma: "
   ).split(",");
@@ -109,11 +128,13 @@ function receivePlayerInput() {
 }
 
 function playGame() {
+  // Condition 1: so far any of the 2d array cell is empty cell, keep prompting for input.
   let numOfTurns = 9;
 
   for (let i = 0; i < numOfTurns; i++) {
-    gameBoard.displayGameBoard();
+    console.log("PlayGame Called");
     if (checkForEmptyCell()) {
+      console.log("A cell is empty");
       receivePlayerInput();
     }
 
---
+++ test.js
@@ -0,0 +1,8 @@
+let text = "1,2";
+const myArray = text.split(",");
+console.log(myArray[0]);
+
+// How do I receive input from the players to play game
+// I need to collect the row and column from the player and check if it is their turn to play
+
+// I have an array where I store my players. Now I want to determine when it is the turn of each palyer to play the game.
